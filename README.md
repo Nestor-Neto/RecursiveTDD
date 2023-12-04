080553509adf1e94b969dcc3a901612558ddee6e
Nestor-Neto <nestor.neto@outlook.com>
2023-12-04 15:03:15 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# ANDRITZ Dev Assessment
+
+This is a evaluation test for developer position.
+
+There are 2 tests:
+
+1: Imagine that we have a subtitle file (.srt) out of sync. The program must be able to shift the whole source file by the given timespan.
+
+2: Given a predefined list of links, the program must be able to calculate the possible paths between one point and another.
+
+# Teste de conhecimento 
+- 1: Imagine que temos um arquivo de legenda (.srt) fora de sincronia. 
+O programa deve ser capaz de deslocar todo o arquivo de origem no intervalo de tempo determinado.
+
+- 2: Dada uma lista predefinida de links, o programa deve ser capaz de calcular os caminhos possiveis entre um ponto e outro.
+# RecursiveTDD
---
+++ SubtitleTimeshift/Shifter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SubtitleTimeshift
+{
+    public class Shifter
+    {
+        async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
+        {
+            Regex regex = new Regex(@"(?<start>\S+)\s-->\s(?<end>\S+)");
+            using (StreamWriter writer = new StreamWriter(output, encoding, bufferSize, leaveOpen))
+            using (StreamReader reader = new StreamReader(input, encoding, true, bufferSize, leaveOpen))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Match m = regex.Match(line);
+                    if (m.Success)
+                    {
+                        string start = m.Groups[1].Value;
+                        string end = m.Groups[2].Value;
+
+                        var newStart = TimeSpan.Parse(start, new CultureInfo("id-ID")).Add(timeSpan).ToString("hh':'mm':'ss'.'fff", new CultureInfo("en-US"));
+                        var newEnd = TimeSpan.Parse(end, new CultureInfo("id-ID")).Add(timeSpan).ToString("hh':'mm':'ss'.'fff", new CultureInfo("en-US"));
+
+                        line = line.Replace(start, newStart);
+                        line = line.Replace(end, newEnd);
+                    }
+                    await writer.WriteLineAsync(line);
+                }
+            }
+        }
+    }
+}
+
